91fd44771e92c9a0cf4d5f92e6deb1cbe191b0bb
Michael Home <samfam921@gmail.com>
2022-07-15 14:10:35 -0700

    more progress

--- script 2.R
+++ script 2.R
@@ -1,5 +1,21 @@
+
+
+
+junk <- read.csv("https://data.chhs.ca.gov/dataset/7a456555-87b9-4830-817c-72d72e628745/resource/f8bb67bf-923d-4d74-8714-dc2bcf5609b7/download/2021-11-29_deaths_final_2014_2020_state_year_sup.csv")
+
+
+
+county_deaths <- read.csv("https://data.chhs.ca.gov/dataset/58619b69-b3cb-41a7-8bfc-fc3a524a9dd4/resource/579cc04a-52d6-4c4c-b2df-ad901c9049b7/download/2021-11-29_deaths_final_2014_2020_county_year_sup.csv")
+
+
+
 idData <- read.csv("https://bit.ly/cali-id")
 
 table(idData$Year)
 
 names(idData)
+
+
+ggplot
+
+
