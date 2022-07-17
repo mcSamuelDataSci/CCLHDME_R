cb358b83c4b4bbea8c207630f06ea490d0bd1212
Michael Home <samfam921@gmail.com>
2022-07-17 11:15:41 -0700

    little clean up

--- script 3.R
+++ script 3.R
@@ -1,15 +1,59 @@
 
-https://github.com/reconverse/outbreaks
-library("outbreaks")
+# https://github.com/reconverse/outbreaks
 
+library(dplyr)
+library(lubridate)
+library(zoo)
+library("outbreaks")
 library(summarytools)
+library(ggplot2)
 
 ebola <- ebola_sierraleone_2014
+
 view(dfSummary(ebola))
 
 junk <- fluH7N9_china_2013
 view(dfSummary(rabies))
 
+
+
+ebola <- ebola %>%
+           mutate(good_date = as.Date(date_of_onset),
+                  good_year = year(date_of_onset),
+                  good_q = as.yearmon(date_of_onset))
+
+ebola1 <- ebola %>% 
+  group_by(district, good_q) %>%
+  summarize(N = n())
+
+
+ebola2 <- ebola %>% 
+           group_by(district, good_q) %>%
+           summarize(N = n())
+
+
+
+
+barplot(ebola2$N, names.arg= ebola2$good_q)
+
+
+
+
+ggplot(data=ebola1, aes(x=good_q, y = N)) + geom_col()
+ggplot(data=ebola2, aes(x=good_q, y = N)) + geom_col()
+
+ggplot(data=ebola1, aes(x=good_q, y = N)) + geom_bar(stat="identity")
+ggplot(data=ebola2, aes(x=good_q, y = N)) + geom_bar(stat="identity")
+
+
+ggplot(data=ebola, aes(x=good_q)) + geom_bar(stat="count") + facet_grid(rows = vars(district))
+
+
+
+
+
+ggplot(data=ebola2, aes(x=good_q, y = N)) + geom_bar(stat="sum")
+ggplot(data=ebola2, aes(x=good_q, y = N)) + geom_bar(stat="mean")
 
 
 
