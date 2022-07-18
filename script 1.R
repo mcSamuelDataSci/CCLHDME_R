4de75b8a8ae7aac5a09c25364458f86152e497cc
kangjaspreet <jaspreetkang@g.ucla.edu>
2022-07-18 13:43:40 -0700

    Update script 1 and 2

--- script 1.R
+++ script 1.R
@@ -1,39 +1,16 @@
-# R Basics
-
-# Mathematical operators ----
-2 + 2 # Addition
-8 - 7 # Subtraction
-2 * 3 # Multiplication
-4 / 3 # Division
-4^2 # 4 to the power of 2
-2 + 3 * 6
-(2 + 3) * 6
-
+# Math operators ----
+(2 + 5) * 4 / 3
 
 # Objects ----
 a <- 8 * 6
 a
-A # R is case sensitive
+A # R is case-sensitive
 A <- 7 * 7
 
-ratePer <- 100000
-
+ratePer <- 1000
 5 / 5000 * ratePer
-3 / 4000 * ratePer
-6 / 6000 * ratePer
-
-# Rules for naming objects ----
-1object <- 1
-object1 <- 1
-object! <- 1
-object name <- 1
-object.name <- 1
-object_name <- 1
-objectName <- 1 #camelCase
-
-# Remove objects ----
-rm(objectName)
-objectName
+4 / 2000 * ratePer
+6 / 7000 * ratePer
 
 # Data classes ----
 
@@ -43,72 +20,92 @@ objectName
 5.8
 
 # Character (text) ----
-"R Workshop"
-'R Workshop'
-'Welcome to the first CCLHDME "R Workshop" '
+"Workshop"
+Workshop
+'Workshop'
 
 # Logical (TRUE, FALSE) ----
 TRUE
 FALSE
 True
-true
-T # TRUE
-F  # FALSE
+false
+T
+F
 
 # Data structures - Vectors ----
-c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) # Numeric vector
-c(1:10) # Shorter way to generate a sequence of values
+
+c(3, 7, 2, 9, 12) # Numeric vector
+c(a, A, ratePer)
 
 c("Alpine", "Alameda", "Butte") # Character vector
-c(Alpine, Alameda, Butte) # Error
-c(object_name, object.name, object1) # Vectors of objects with single values of same type
 
 c(TRUE, FALSE) # Logical vector
 
 c(1, "Alpine", TRUE) # Converts elements to character type
 c(1, TRUE, FALSE) # Converts to numeric type - Underlying integer for TRUE is 1, FALSE is 0
 
-myVector <- c(2, 3, 4) # Store vectors as objects
+myVector <- c(4, 10, 8) # Store vectors as objects
 
 myVector ^ 2 # Operations on a vector
 myVector + 3
 myVector + myVector
 
-myVector <- c("Alameda", "Alpine", "Butte")
-myVector + "County"
-paste(myVector, "County")
-
 # Subsetting vectors ----
-myVector[1] # Return 1st value
-myVector[4] # Returns NA (Not Available) since vector only has 3 values
-myVector[-1] # Return all values except the first
-myVector[c(1, 2)] # Return 1st and 2nd value
+# myVector[1] # Return 1st value
+# myVector[4] # Returns NA (Not Available) since vector only has 3 values
+# myVector[-1] # Return all values except the first
+# myVector[c(1, 2)] # Return 1st and 2nd value
 
 # Functions ----
-values <- 1:50
+values <- c(1, 2, 3, 4, 5)
 values
 length(values) # How many values are in the vector
 sum(values) # Sums up all values in the vector
 sum(values) / length(values) # Use two functions above to calculate mean
 mean(values) # Or use mean function
 
-?mean # View documentation of mean function
+NA # Not available
 
-values <- c(1:50, NA) # Add missing value (NA) to vector
+values <- c(1, 2, 3, 4, 5, NA) # Add missing value (NA) to vector
 values
 
 mean(values) # Returns NA due to one NA value
+
+?mean # View documentation of mean function
+
 mean(values, na.rm = TRUE) # Specify na.rm argument to TRUE, which removes NA value, then computes mean
 
-x_values <- 1:50
+x_values <- 1:5
 x_values
 y_values <- x_values * 2
 y_values
 
-?plot
 plot(x = x_values, y = y_values)
-plot(x = x_values, y = y_values, type = "l")
-plot(x = x_values, y = y_values, type = "p", 
+?plot
+plot(x = x_values, y = y_values, type = "l", 
      main = "Plot", xlab = "X values", ylab = "Y values")
 
-# Data structures - Data frame ----
+# Data structures - Data frames ----
+county <- rep("Alameda", 3)
+sex <- c("Female", "Male", "Total")
+n <- c(4000, 6000, 10000)
+population <- c(800000, 700000, 1500000)
+
+# Create data frame
+myData <- data.frame(County = county,
+                     Sex = sex,
+                     N = n, 
+                     Population = population)
+myData # Print out data frame in the R console
+View(myData) # View data frame in R Studio
+
+myData$N # $ operator, select specific column
+myData$rate # rate column doesn't exist
+
+# Create column that stores crude rates per 100k population
+myData$Rate <- myData$N / myData$Population * ratePer
+myData
+View(myData)
+
+str(myData)
+
--- script 2.R
+++ script 2.R
@@ -1,21 +1,32 @@
+# junk <- read.csv("https://data.chhs.ca.gov/dataset/7a456555-87b9-4830-817c-72d72e628745/resource/f8bb67bf-923d-4d74-8714-dc2bcf5609b7/download/2021-11-29_deaths_final_2014_2020_state_year_sup.csv")
+# county_deaths <- read.csv("https://data.chhs.ca.gov/dataset/58619b69-b3cb-41a7-8bfc-fc3a524a9dd4/resource/579cc04a-52d6-4c4c-b2df-ad901c9049b7/download/2021-11-29_deaths_final_2014_2020_county_year_sup.csv")
 
+# Reading in data ----
+# .Rdata files
+# .RDS files
+# .csv files
+# .xlsx
+# .txt
+# .sas7dat
+# .json
+# ....
 
+idData <- read.csv("https://bit.ly/cali-id") # Read in csv file
 
-junk <- read.csv("https://data.chhs.ca.gov/dataset/7a456555-87b9-4830-817c-72d72e628745/resource/f8bb67bf-923d-4d74-8714-dc2bcf5609b7/download/2021-11-29_deaths_final_2014_2020_state_year_sup.csv")
+View(idData) # View the data frame
+names(idData) # View the column names
+str(idData) # View the structure
+unique(idData$Disease) # Unique values of columns
+unique(idData$County)
+unique(idData$Sex)
 
+table(idData$Year) # Frequency table
 
+# Install packages ----
+install.packages("ggplot2") # Popular visualization package
+library(ggplot2)
 
-county_deaths <- read.csv("https://data.chhs.ca.gov/dataset/58619b69-b3cb-41a7-8bfc-fc3a524a9dd4/resource/579cc04a-52d6-4c4c-b2df-ad901c9049b7/download/2021-11-29_deaths_final_2014_2020_county_year_sup.csv")
-
-
-
-idData <- read.csv("https://bit.ly/cali-id")
-
-table(idData$Year)
-
-names(idData)
-
-
-ggplot
+install.packages("dplyr") # Popular data wrangling package, part of tidyverse suite
+library(dplyr)
 
 
