2d308f8e3fdda281ba3e3b9a365b2cfcc8788400
Michael Home <samfam921@gmail.com>
2022-07-19 15:57:08 -0700

    intro and script 3 updates

--- script 3.R
+++ script 3.R
@@ -19,53 +19,74 @@ library("outbreaks")
 
 # Note: all data "wrangling here using tidyverse dplyr
 
-full_data <- ebola_sierraleone_2014  # get "built-in" data set
 
-view(dfSummary(full_data))
 
-# filter rows based on condition
+# get "built-in" data set from outbreak package
+full_data <- ebola_sierraleone_2014  
+
+# click on full_data in environment 
+
+view(dfSummary(full_data)) # from summarytools package
+
+
+
+# *filter* rows based on condition
 just_confirmed <- filter(ebolaOutbreak0, status == "confirmed")    
-just_confirmed <- ebolaOutbreak0 %>% filter(status == "confirmed") # same exact thing
+just_confirmed <- ebolaOutbreak0 %>% filter(status == "confirmed") 
 
-# select columns and rename a couple at the same time
+# *select* columns and rename a couple at the same time
 just_some_variables <- full_data %>% 
                         select(age, status, onset = date_of_onset, district )
 
-full_data_plus  <- full_data %>%
-                    mutate(onset_yr_mo = as.yearmon(date_of_onset))  # zoo package; make year_month variable
 
-# "piping" the steps above into one unit with %>%
+# *mutate* to create new variables
+# as.yearmon() function from zoo package
+full_data_plus  <- full_data %>%
+                    mutate(onset_yr_mo = as.yearmon(date_of_onset))  
+
+
+
+# "piping" the steps above into one step with %>%
 good_data  <- ebolaOutbreak0 %>% 
                  filter(status == "confirmed") %>%
                  select(age, status, onset = date_of_onset, district ) %>%
                  mutate(onset_yr_mo = as.yearmon(onset))  
 
-# grouping by district and then county cases in each district
+
+
+# grouping by (group_by) district 
+# count cases in each district with summarize
 district_data <- good_data  %>% 
                    group_by(district) %>%
-                   summarize(N   = n(),
-                             age = round(mean(age, na.rm=TRUE),1)  )
+                   summarize(N   = n())
 
+# simple chart with base R
 barplot(district_data$N, names.arg= district_data$district)
 
-
+# pretty simple bar chart with ggplot
 ggplot(data=district_data, aes(x=district, y = N)) + # core ggplot set up
-   geom_bar(stat="identity", fill="darkblue") +                    # geom = what type of plot
-   coord_flip() +
-   labs(y="Number of Confirmed Cases")
+   geom_bar(stat="identity", fill="darkblue") +      # geom = what type of plot
+   coord_flip() +                                    # "horizontal" bar chart
+   labs(y="Number of Confirmed Cases")               # axis label
 
 
+
+# grouping by (group_by) district and year-month 
 district_time_data  <- good_data %>% 
   group_by(district, onset_yr_mo) %>%
   summarize(N = n() )
 
-# core ggplot set up
+# line chart...
 ggplot(data=district_time_data, aes(x=onset_yr_mo, y = N, col=district )) + 
        geom_line(size=1.2)
 
 
+
+# read in Sierra Leone population data
 pop_dat <- read.csv("sierra_leone.csv")
 
+# join (merge) case data with population data 
+# calculate rate with mutate
 rate_data  <- full_join(district_data, pop_dat, by= c("district" = "District")) %>%
                 mutate(Rate = N*100000/Population)
 
